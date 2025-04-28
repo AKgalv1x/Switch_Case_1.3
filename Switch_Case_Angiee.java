c300ebb05ae706d225c3063d8fe153921cbe6333
Angiee Karoline Galvis Tarazona <akgalvis@gmail.com>
2025-04-28 08:51:25 -0500

    Update Switch_Case_Angiee.java
    Se cambiaron los datos del ejemplo de la clase (días de la semana) a el ejemplo práctico seleccionado (comidas)

--- Switch_Case_Angiee.java
+++ Switch_Case_Angiee.java
@@ -22,12 +22,12 @@ public class Switch_Case_Angiee {
         
         switch (dia){
                 case 1 -> System.out.println("Ramen de pollos y setas");
-                case 2 -> System.out.println("martes");
-                case 3 -> System.out.println("miercoles");
-                case 4 -> System.out.println("jueves");
-                case 5 -> System.out.println("viernes");
-                case 6 -> System.out.println("sabado");
-                case 7 -> System.out.println("domingo");
+                case 2 -> System.out.println("Pancakes con miel y fresas");
+                case 3 -> System.out.println("Bandeja Paisa");
+                case 4 -> System.out.println("Arroz con leche");
+                case 5 -> System.out.println("Empanadas");
+                case 6 -> System.out.println("Galletas con leche");
+                case 7 -> System.out.println("Arroz chino");
                 default -> System.out.println("Número de dia invalido");
         }
 
